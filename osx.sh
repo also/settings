54dd2a4353d5413516b7dc6c7292f9929a76b6fe
Ryan Berdeen <ryan@ryanberdeen.com>
2011-10-06 15:04:47 -0400

    add some os x settings

---
+++ osx.sh
@@ -0,0 +1,10 @@
+# https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+
+# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
