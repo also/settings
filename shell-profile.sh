8deb8fc89ed95c08d7057f86cb887a4a1718fa02
Ryan Berdeen <ryan@ryanberdeen.com>
2016-03-12 13:09:09 -0500

    no grep color

--- shell-profile.sh
+++ shell-profile.sh
@@ -1,7 +1,5 @@
 export CLICOLOR=1
 
-export GREP_OPTIONS="--color=auto -n"
-
 export PATH=$HOME/.rbenv/bin:$HOME/.jenv/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/usr/local/git/bin:$HOME/Library/Haskell/bin
 
 eval "$(rbenv init -)"
