a65c0dc9dc81a4d58999f1cfa8848aca09f4129a
Ryan Berdeen <ryan@ryanberdeen.com>
2016-06-11 18:46:05 -0400

    prompt

--- shell-profile.sh
+++ shell-profile.sh
@@ -7,3 +7,34 @@ eval "$(jenv init -)"
 if which nodenv > /dev/null; then eval "$(nodenv init -)"; fi
 
 source $HOME/work/settings/helpers.sh
+
+function prompt_git() {
+  # check if we're in a git repo. (fast)
+  git rev-parse --is-inside-work-tree &>/dev/null || return
+
+  branchName="$(git symbolic-ref --quiet --short HEAD 2> /dev/null || \
+    git describe --all --exact-match HEAD 2> /dev/null || \
+    git rev-parse --short HEAD 2> /dev/null || \
+    echo '(unknown)')";
+
+  echo " on $branchName"
+}
+
+function set_prompts() {
+  local bold=$(tput bold)
+  local green=$(tput setaf 190)
+  local purple=$(tput setaf 141)
+  local reset=$(tput sgr0)
+  local black=$(tput setaf 0)
+
+  PS1="\[\033]0;\w\007\]"                                 # terminal title (set to the current working directory)
+  PS1+="\n\[$bold\]"
+  PS1+="\[$green\]\w"                                     # working directory
+  PS1+="\[$purple\]\$(prompt_git)"
+  PS1+="\n"
+  PS1+="\[$reset\]\\$ "
+}
+
+
+set_prompts
+unset set_prompts
